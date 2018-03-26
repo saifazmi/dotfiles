41be89607d7ea8c519a754f066f21ff2a48af88a
Saif Azmi <saif.azmi0108@gmail.com>
2018-03-26 17:52:32 +0100

    [EDITORS] backup editor settings

--- backup.sh
+++ backup.sh
@@ -16,30 +16,41 @@ echo "==================="
 echo
 echo "Copying config files..."
 cd $HOME
-echo "// dunst"
-cp --parents .config/dunst/* dotfiles
+echo "==== Window Manager ===="
 echo "// i3wm"
 cp --parents .config/i3/* dotfiles
+cp --parents /usr/lib/i3blocks/battery dotfiles
+echo "// rofi"
+cp --parents .config/rofi/* dotfiles
+echo "// dunst"
+cp --parents .config/dunst/* dotfiles
 echo "// redshift"
 cp --parents .config/redshift.conf dotfiles
-echo "// rofi"
-cp --parents .config/rofi/* ~/dotfiles
+
+echo "==== Dev Tools ===="
 echo "// sublime text 3"
 cp --parents .config/sublime-text-3/Packages/User/*.sublime-settings dotfiles
-echo "// gitconfig"
-cp .gitconfig dotfiles
-echo "// oh-my-zsh"
-cp --parents .oh-my-zsh/themes/classyTouch.zsh-theme dotfiles
+echo "// vs code"
+cp --parents .config/Code/CachedExtensions/user dotfiles
+cp --parents .config/Code/User/settings.json dotfiles
 echo "// vim"
 cp .vimrc dotfiles
+echo "// gitconfig"
+cp .gitconfig dotfiles
+
+echo "==== Terminal ===="
+echo "// zsh"
+cp .zprofile dotfiles
+cp .zshrc dotfiles
+echo "// oh-my-zsh"
+cp --parents .oh-my-zsh/themes/classyTouch.zsh-theme dotfiles
+
+echo "==== Xorg ===="
 echo "// xinit"
 cp .xinitrc dotfiles
 echo "// Xresources"
 cp .Xresources dotfiles
 cp --parents .Xresources.d/* dotfiles
-echo "// zsh"
-cp .zprofile dotfiles
-cp .zshrc dotfiles
 echo
 echo "Done copying config files"
 echo "git status ~/dotfiles"
