bae2e92d1c03b450bbd4f32991c7d98cf1a8eaa1
Saif Azmi <saifazmi.dev@gmail.com>
2024-02-16 19:16:41 +0000

    add harpoon plugin

---
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/harpoon.lua
@@ -0,0 +1,28 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = {
+    'nvim-lua/plenary.nvim'
+  },
+
+  config = function()
+    local harpoon = require('harpoon')
+
+    harpoon:setup() -- required for autocmds setup
+
+    local keymap = vim.keymap -- for conciceness
+
+    -- keybindings
+    keymap.set('n', '<leader>ha', function() harpoon:list():append() end)
+    keymap.set('n', '<leader>hs', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+    keymap.set('n', '<leader>1', function() harpoon:list():select(1) end)
+    keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
+    keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
+    keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<leader>hp', function() harpoon:list():prev() end)
+    vim.keymap.set('n', '<leader>hn', function() harpoon:list():next() end)
+  end
+}
