f271b2c37cf5f9353626675de10b71074f12d904
Saif Azmi <saifazmi.dev@gmail.com>
2024-02-16 20:13:28 +0000

    add a sync.sh script

---
+++ sync.sh
@@ -0,0 +1,20 @@
+# =============================================================================
+# Dotfiles backup script
+# =============================================================================
+
+echo "---------------------------------------"
+echo "Backing up: Development Environment    "
+echo "---------------------------------------"
+echo "\nSyncronising ~/.zshrc ..."
+rsync -av  ~/.zshrc ./commons/dev-environment/
+echo "\nSyncronising ~/.vimrc ..."
+rsync -av  ~/.vimrc ./commons/dev-environment/
+echo "\nSyncronising ~/.gitconfig ..."
+rsync -av  ~/.gitconfig ./commons/dev-environment/
+echo "\nSyncronising ~/.config/tmux ..."
+# will probably add tmux-powerline config here later
+rsync -av  ~/.config/tmux/tmux.conf ./commons/dev-environment/.config/tmux/
+echo "\nSyncronising ~/.config/nvim ..."
+rsync -av  ~/.config/nvim/ ./commons/dev-environment/.config/nvim/
+
+
