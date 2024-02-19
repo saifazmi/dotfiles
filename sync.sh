a1f956774020d097a266f118493158ec184a3e71
Saif Azmi <saifazmi.dev@gmail.com>
2024-02-19 01:07:13 +0000

    update sync script

--- sync.sh
+++ sync.sh
@@ -7,14 +7,14 @@ echo "Backing up: Development Environment    "
 echo "---------------------------------------"
 echo "\nSyncronising ~/.zshrc ..."
 rsync -av  ~/.zshrc ./commons/dev-environment/
+echo "\nSyncronising ~/.p10k.zsh ..."
+rsync -av ~/.p10k.zsh ./commons/dev-environment/
 echo "\nSyncronising ~/.vimrc ..."
 rsync -av  ~/.vimrc ./commons/dev-environment/
 echo "\nSyncronising ~/.gitconfig ..."
 rsync -av  ~/.gitconfig ./commons/dev-environment/
 echo "\nSyncronising ~/.config/tmux ..."
-# will probably add tmux-powerline config here later
 rsync -av  ~/.config/tmux/tmux.conf ./commons/dev-environment/.config/tmux/
 echo "\nSyncronising ~/.config/nvim ..."
 rsync -av  ~/.config/nvim/ ./commons/dev-environment/.config/nvim/
-
 
