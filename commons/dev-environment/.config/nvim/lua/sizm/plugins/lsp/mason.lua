97b0b08686c5541ae61eb5015c01c1bb84e15a87
Saif Azmi <saifazmi.dev@gmail.com>
2024-02-29 12:47:01 +0100

    [nvim] feature: add mason-tool-installer to manage linters and formatters

--- commons/dev-environment/.config/nvim/lua/sizm/plugins/lsp/mason.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/lsp/mason.lua
@@ -1,21 +1,24 @@
 return {
+  -- install and manage language tools (like: LSP, linters, formatters, etc)
   'williamboman/mason.nvim',
   dependencies = {
     'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
   },
 
   config = function()
     local mason = require('mason')
     local mason_lspconfig = require('mason-lspconfig')
+    local mason_tool_installer = require('mason-tool-installer')
 
     mason.setup({
       ui = {
         icons = {
           package_installed = '✓',
           package_pending = '➜',
-          package_uninstalled = '✗'
-        }
-      }
+          package_uninstalled = '✗',
+        },
+      },
     })
 
     mason_lspconfig.setup({
@@ -25,11 +28,23 @@ return {
         'emmet_ls',
         'cssls',
         'tailwindcss',
-        'solargraph'
+        'pyright', -- python
+        'solargraph', -- ruby
       },
       -- auto-install configured servers (with lspconfig)
-      automatic_installtion = true -- not the same as ensure_installed
+      automatic_installtion = true, -- not the same as ensure_installed
     })
-  end
-}
 
+    mason_tool_installer.setup({
+      ensure_installed = {
+        'stylua', -- lua formatter
+        'prettier', -- web dev frontend stack formatter
+        'eslint_d', -- javascript & typescript linter
+        'isort', -- python imports sorter (formatter)
+        'black', -- python formatter
+        'pylint', -- python linter
+        'rubocop', -- ruby linter and formatter
+      },
+    })
+  end,
+}
