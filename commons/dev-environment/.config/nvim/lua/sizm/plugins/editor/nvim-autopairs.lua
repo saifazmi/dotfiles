e8dd9fbfae0e21cd34f8a14ad93d31ab8bc77d86
Saif Azmi <saifazmi.dev@gmail.com>
2024-02-29 12:44:28 +0100

    [nvim] feature: enable tressiter for autopairs

--- commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/nvim-autopairs.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/nvim-autopairs.lua
@@ -1,6 +1,31 @@
 return {
+  -- auto close brackets
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
 
-  config = true -- runs require('nvim-autopairs').setup()
+  config = function()
+    local npairs = require('nvim-autopairs')
+
+    npairs.setup({
+      enable_check_bracket_line = false,
+      check_ts = true, -- enable treesitter
+      ts_config = {
+        -- don't add pairs in lua string treesitter nodes
+        lua = { 'string' },
+        -- don't add pairs in javascript template_string treesitter nodes
+        javascript = { 'template_string' },
+        -- don't check treesitter in java
+        java = false,
+      },
+    })
+
+    -- import nvim-autopairs completion functionality
+    local npairs_cmp = require('nvim-autopairs.completion.cmp')
+
+    -- import nvim-cmp plugin (completions plugin)
+    local cmp = require('cmp')
+
+    -- make autopairs and completion work together
+    cmp.event:on('confirm_done', npairs_cmp.on_confirm_done())
+  end,
 }
