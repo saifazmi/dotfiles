4b221c698e647c0638796e3a267bb00aeccdad68
Saif Azmi <saifazmi.dev@gmail.com>
2024-03-06 11:03:12 +0000

    [nvim] feat: add trouble.nvim plugin to see diagnostics in a single buffer

---
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/trouble.lua
@@ -0,0 +1,9 @@
+return {
+  -- list of diagnostics, LSP ref, qfix list, etc.
+  'folke/trouble.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+  opts = {
+    use_diagnostic_signs = true,
+  },
+}
