2791296afa824b327bcd987429f887c396e0cdf7
Saif Azmi <saifazmi.dev@gmail.com>
2024-05-03 14:46:28 +0200

    [nvim] feature: add Trouble plugin

--- commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/trouble.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/trouble.lua
@@ -2,6 +2,15 @@ return {
   -- list of diagnostics, LSP ref, qfix list, etc.
   'folke/trouble.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  cmd = {
+    'Trouble',
+    'TroubleToggle',
+    'TroubleClose',
+    'TroubleRefresh',
+  },
+  keys = {
+    { '<leader>t', '<cmd>TroubleToggle<CR>' },
+  },
 
   opts = {
     use_diagnostic_signs = true,
