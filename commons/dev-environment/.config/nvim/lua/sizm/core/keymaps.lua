eadf7fc4f75778ee7f0ab672ea4856746116c1e8
Saif Azmi <saifazmi.dev@gmail.com>
2024-05-03 14:44:40 +0200

    [nvim] feature: add keymap for deleting buffer

--- commons/dev-environment/.config/nvim/lua/sizm/core/keymaps.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/core/keymaps.lua
@@ -1,16 +1,19 @@
-local keymap = vim.keymap                   -- for conciseness
+local keymap = vim.keymap -- for conciseness
 
-vim.g.mapleader = ' '                       -- remap <leader> to Space Bar key
+vim.g.mapleader = ' ' -- remap <leader> to Space Bar key
 
-keymap.set('n', '<leader>e', vim.cmd.Ex)    -- file explorer
+keymap.set('n', '<leader>e', vim.cmd.Ex) -- file explorer
+
+-- buffer management
+keymap.set('n', '<leader>bd', ':bd<CR>') -- delete buffer
 
 -- window management
-keymap.set('n', '<leader>sv', '<C-w>v')     -- split window vertically
-keymap.set('n', '<leader>sh', '<C-w>s')     -- split window horizontally
-keymap.set('n', '<leader>se', '<C-w>=')     -- make splits equal width & height
-keymap.set('n', '<leader>k', '4<C-w>+')     -- increase split height by 1 line
-keymap.set('n', '<leader>j', '4<C-w>-')     -- decrease split height by 1 line
-keymap.set('n', '<leader>l', '8<C-w>>')     -- increase split width by 1 column
-keymap.set('n', '<leader>h', '8<C-w><')     -- decrease split width by 1 column
+keymap.set('n', '<leader>sv', '<C-w>v') -- split window vertically
+keymap.set('n', '<leader>sh', '<C-w>s') -- split window horizontally
+keymap.set('n', '<leader>se', '<C-w>=') -- make splits equal width & height
 keymap.set('n', '<leader>sx', ':close<CR>') -- close current split window
 
+keymap.set('n', '<leader>k', '4<C-w>+') -- increase split height by 1 line
+keymap.set('n', '<leader>j', '4<C-w>-') -- decrease split height by 1 line
+keymap.set('n', '<leader>l', '8<C-w>>') -- increase split width by 1 column
+keymap.set('n', '<leader>h', '8<C-w><') -- decrease split width by 1 column
