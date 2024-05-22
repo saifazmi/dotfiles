6363df2ba668cfb2b6c4ac7a02607d5d0624c023
Saif Azmi <saifazmi.dev@gmail.com>
2024-05-22 20:07:51 +0100

    fix: echo not interpreting backslash escape for newline in backup.sh
    caused by using #!/usr/bin/env bash

--- backup.sh
+++ backup.sh
@@ -8,19 +8,19 @@ echo "---------------------------------------"
 echo "Backing up: Development Environment    "
 echo "---------------------------------------"
 
-echo "\nBacking up ~/.zshrc ..."
+echo -e "\nBacking up ~/.zshrc ..."
 rsync -av  ~/.zshrc ./commons/dev-environment/
-echo "\nBacking up ~/.p10k.zsh ..."
+echo -e "\nBacking up ~/.p10k.zsh ..."
 rsync -av ~/.p10k.zsh ./commons/dev-environment/
-echo "\nBacking up ~/.vimrc ..."
+echo -e "\nBacking up ~/.vimrc ..."
 rsync -av  ~/.vimrc ./commons/dev-environment/
-echo "\nBacking up ~/.gitconfig ..."
+echo -e "\nBacking up ~/.gitconfig ..."
 rsync -av  ~/.gitconfig ./commons/dev-environment/
-echo "\nBacking up ~/.gitignore_global ..."
+echo -e "\nBacking up ~/.gitignore_global ..."
 rsync -av  ~/.gitignore_global ./commons/dev-environment/
-echo "\nBacking up ~/.config/tmux ..."
+echo -e "\nBacking up ~/.config/tmux ..."
 rsync -av  ~/.config/tmux/tmux.conf ./commons/dev-environment/.config/tmux/
-echo "\nBacking up ~/.config/nvim ..."
+echo -e "\nBacking up ~/.config/nvim ..."
 rsync -av --delete  ~/.config/nvim/ ./commons/dev-environment/.config/nvim/
 
 echo "---------------------------------------"
