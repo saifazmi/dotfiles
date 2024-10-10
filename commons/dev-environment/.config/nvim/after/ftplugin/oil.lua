d3b0097efd6a71fabdf60834e2b97179c9b182af
Saif Azmi <saifazmi.dev@gmail.com>
2024-10-10 11:31:33 +0100

    [nvim] fix: oil plugin and TmuxNavigateLeft conflict

--- commons/dev-environment/.config/nvim/after/ftplugin/oil.lua
+++ commons/dev-environment/.config/nvim/after/ftplugin/oil.lua
@@ -1,1 +1,2 @@
 vim.keymap.set('n', '<c-l>', '<cmd>TmuxNavigateRight<CR>', { silent = true, buffer = true })
+vim.keymap.set('n', '<c-h>', '<cmd>TmuxNavigateLeft<CR>', { silent = true, buffer = true })
