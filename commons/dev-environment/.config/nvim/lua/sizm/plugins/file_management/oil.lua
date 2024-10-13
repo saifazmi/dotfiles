95cb7a11d33bd50cef8cd5dce8c3e78148f6649a
Saif Azmi <saifazmi.dev@gmail.com>
2024-10-14 00:17:32 +0100

    [nvim] refactor: cleanup netrw and Oil configs
    - disabling the workaround for netrw and vim-tmux-navigator
    - add new keymap for Oil refresh to see any new files created outside
    nvim
    - chagne to Oil, autorefersh on change and delete to trash
    - disable Oil default explorer, wasn't using anyway as it was on keymap
    load
    - general cleanup for consistency

--- commons/dev-environment/.config/nvim/lua/sizm/plugins/file_management/oil.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/file_management/oil.lua
@@ -10,6 +10,14 @@ return {
   },
 
   opts = {
+    default_file_explorer = false,
+    delete_to_trash = true,
+    watch_for_changes = true,
+    keymaps = {
+      -- fix: for vim-tmux-navigator conflict similar to netrw
+      ['<C-h>'] = false, -- disable to fix TmuxNavigateLeft conflict
+      ['<C-l>'] = false, -- disable to fix TmuxNavigateRight conflict
+    },
     view_options = { show_hidden = true },
   },
 
