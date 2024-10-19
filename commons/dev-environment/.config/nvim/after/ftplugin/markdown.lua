7042b7333fcd668642f8950130b255628adfcb35
Saif Azmi <saifazmi.dev@gmail.com>
2024-10-19 16:38:53 +0100

    [nvim] feature: markdown file settings

---
+++ commons/dev-environment/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,12 @@
+-- indent size
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
+
+-- spell check
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_gb', 'en_us' }
+
+-- markdown editing utils
+vim.opt.conceallevel = 2
+vim.opt.wrap = true
