033fdbef115e79c831312d66700b42866392dd8d
Saif Azmi <saifazmi.dev@gmail.com>
2024-11-04 00:13:19 +0000

    [nvim] refactor: move indent line plugins in one file to share exclude patterns

--- commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/indent-blankline.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/indent-blankline.lua
@@ -1,30 +1,59 @@
-return {
-  -- add indent lines to code block
-  'lukas-reineke/indent-blankline.nvim',
-  event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
-  main = 'ibl',
+local indent_symbol = '│'
+local exclude_filetypes = {
+  'help',
+  'lazy',
+  'mason',
+  'oil',
+  'oil_preview',
+  'dashboard',
+  'undotree',
+  'Trouble',
+  'trouble',
+}
 
-  opts = {
-    indent = {
-      char = '│',
-      tab_char = '│',
-    },
-    whitespace = {
-      remove_blankline_trail = true,
-    },
-    scope = { enabled = false },
-    exclude = {
-      filetypes = {
-        'help',
-        'lazy',
-        'mason',
-        'oil',
-        'oil_preview',
-        'dashboard',
-        'undotree',
-        'Trouble',
-        'trouble',
+return {
+  {
+    -- add indent lines to code block
+    'lukas-reineke/indent-blankline.nvim',
+    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+    main = 'ibl',
+
+    opts = {
+      indent = {
+        char = indent_symbol,
+        tab_char = indent_symbol,
+      },
+      whitespace = {
+        remove_blankline_trail = true,
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = exclude_filetypes,
       },
     },
+  },
+
+  {
+    -- for fancy animated indent line highlight
+    'echasnovski/mini.indentscope',
+    version = '*', -- stable
+    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
+
+    opts = {
+      symbol = indent_symbol,
+      options = {
+        border = 'top',
+        try_as_border = true,
+      },
+    },
+
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = exclude_filetypes,
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
   },
 }
