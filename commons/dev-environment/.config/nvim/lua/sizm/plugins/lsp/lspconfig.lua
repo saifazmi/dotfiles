bd9421f2ab60a485a699f10c1549777ccc1563a9
Saif Azmi <saifazmi.dev@gmail.com>
2025-02-21 12:45:32 +0000

    [nvim] feature: update go lsp settings
    ref:
    
    -
    https://github.com/golang/tools/blob/master/gopls/doc/settings.md#useplaceholders-bool
    -
    https://github.com/golang/tools/blob/master/gopls/doc/analyzers.md#unusedparams-check-for-unused-parameters-of-functions

--- commons/dev-environment/.config/nvim/lua/sizm/plugins/lsp/lspconfig.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/lsp/lspconfig.lua
@@ -149,6 +149,10 @@ return {
       settings = {
         gopls = {
           completeUnimported = true,
+          usePlaceholders = true,
+          analyses = {
+            unusedparams = true,
+          },
         },
       },
     })
