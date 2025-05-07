66facf00c928102cbda6e5cfdd18fab78d1ffe0d
Saif Azmi <saifazmi.dev@gmail.com>
2025-05-08 00:52:20 +0100

    [nvim] feature: cwd and local buffer keymaps for trouble window
    - also removed mini.icons

--- commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/trouble.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/editor/trouble.lua
@@ -3,16 +3,21 @@ return {
   'folke/trouble.nvim',
   dependencies = {
     'nvim-tree/nvim-web-devicons',
-    'echasnovski/mini.icons',
+    -- 'echasnovski/mini.icons',
   },
 
   cmd = 'Trouble',
 
   keys = {
     {
-      '<leader>t',
+      '<leader>tt',
+      '<cmd>Trouble diagnostics toggle filter.buf=0<CR>',
+      desc = 'Toggle Diagnostics for buffer',
+    },
+    {
+      '<leader>tT',
       '<cmd>Trouble diagnostics toggle<CR>',
-      desc = 'Toggle Trouble (diagnostics) window',
+      desc = 'Toggle Diagnostics for cwd',
     },
   },
 
