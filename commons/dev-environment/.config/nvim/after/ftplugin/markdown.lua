a6807a6d2c08520744d0170e0bbdc75a35051a79
Saif Azmi <saifazmi.dev@gmail.com>
2025-05-23 23:17:47 +0100

    [nvim] feature: live markdown rendering in buffer
    - added new plugin `render-markdown`
    - update to markdown filetype plugins

---
+++ commons/dev-environment/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,15 @@
+-- indent size
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
+
+-- ruler
+vim.opt_local.colorcolumn = '80,120'
+
+-- spell check
+vim.opt_local.spell = true
+vim.opt_local.spelllang = { 'en_gb', 'en_us' }
+
+-- markdown editing utils
+vim.opt_local.conceallevel = 2
+vim.opt_local.wrap = false
