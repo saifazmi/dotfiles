5ebc1f71b2f02f0e0c04564b3e6a4e944400e8cb
Saif Azmi <saifazmi.dev@gmail.com>
2025-12-17 19:31:13 +0000

    [tmux] feature: upgrade from catppuccin/tmux theme v0.3.0 to main
    - upin plugin version
    - update to a new theme
    - moved theme details to separate file (see tmux.catppuccin.conf)
    - new plugin for online status
    - update to synch.sh to include the new .conf file

--- sync.sh
+++ sync.sh
@@ -6,19 +6,19 @@ echo "---------------------------------------"
 echo "Backing up: Development Environment    "
 echo "---------------------------------------"
 echo "\nSyncronising ~/.zshrc ..."
-rsync -av  ~/.zshrc ./commons/dev-environment/
+rsync -av ~/.zshrc ./commons/dev-environment/
 echo "\nSyncronising ~/.p10k.zsh ..."
 rsync -av ~/.p10k.zsh ./commons/dev-environment/
 echo "\nSyncronising ~/.vimrc ..."
-rsync -av  ~/.vimrc ./commons/dev-environment/
+rsync -av ~/.vimrc ./commons/dev-environment/
 echo "\nSyncronising ~/.gitconfig ..."
-rsync -av  ~/.gitconfig ./commons/dev-environment/
+rsync -av ~/.gitconfig ./commons/dev-environment/
 echo "\nSyncronising ~/.gitignore_global ..."
-rsync -av  ~/.gitignore_global ./commons/dev-environment/
+rsync -av ~/.gitignore_global ./commons/dev-environment/
 echo "\nSyncronising ~/.config/tmux ..."
-rsync -av  ~/.config/tmux/tmux.conf ./commons/dev-environment/.config/tmux/
+rsync -av ~/.config/tmux/*.conf ./commons/dev-environment/.config/tmux/
 echo "\nSyncronising ~/.config/nvim ..."
-rsync -av --delete  ~/.config/nvim/ ./commons/dev-environment/.config/nvim/
+rsync -av --delete ~/.config/nvim/ ./commons/dev-environment/.config/nvim/
 
 echo "---------------------------------------"
 echo "Git Status"
