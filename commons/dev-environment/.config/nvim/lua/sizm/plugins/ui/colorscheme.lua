b014a446df3579bba16ff2dd2dd3096a3301f6d7
Saif Azmi <saifazmi.dev@gmail.com>
2025-12-29 20:03:50 +0000

    [nvim] feature: add tokyonight as a second colorscheme

--- commons/dev-environment/.config/nvim/lua/sizm/plugins/ui/colorscheme.lua
+++ commons/dev-environment/.config/nvim/lua/sizm/plugins/ui/colorscheme.lua
@@ -1,21 +1,36 @@
 return {
   -- colorschemes for editor
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  lazy = false,
-  priority = 1000,
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    lazy = false,
+    priority = 1000,
 
-  config = function()
-    require('catppuccin').setup({
-      transparent_background = true, -- disables setting the background color.
-      dim_inactive = {
-        enabled = true, -- dims the background color of inactive window
-        shade = 'light',
-        percentage = 0.30, -- percentage of the shade to apply to the inactive window
-      },
-    })
+    config = function()
+      require('catppuccin').setup({
+        transparent_background = true, -- disables setting the background color.
+        dim_inactive = {
+          enabled = true, -- dims the background color of inactive window
+          shade = 'light',
+          percentage = 0.30, -- percentage of the shade to apply to the inactive window
+        },
+      })
 
-    -- setup must be called before loading
-    vim.cmd.colorscheme('catppuccin')
-  end,
+      -- setup must be called before loading
+      vim.cmd.colorscheme('catppuccin')
+    end,
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+
+    config = function()
+      require('tokyonight').setup({
+        style = 'night', -- night, storm, day, moon
+        transparent = true, -- disables setting the background color.
+        dim_inactive = true,
+      })
+    end,
+  },
 }
